7b7e01e05bfd9d6506a253067b86b74d48cddf17
Retrojb <baltes.26@osu.edu>
2018-11-07 17:18:21 -0500

    Inital build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountHomeComponent } from './account/account-home/account-home.component';
+import { OrderHistoryComponent } from './account/order-history/order-history.component';
+import { AddressbookComponent } from './account/addressbook/addressbook.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'catalog', component: CatalogComponent},
+  {path: 'account', loadChildren: 'app/account/account.module#AccountModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
